d8fb828cf58c2e31f3db07658841256835c442cc
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-17 16:19:04 +0100

    refactored all funcs

--- controllers/api/books.js
+++ controllers/api/books.js
@@ -1,50 +1,129 @@
+const { triggerAsyncId } = require("async_hooks");
 const Book = require("../../models/Book");
 
+const books = [{
+    title: 'Make It Stick: The Science of Successful Learning',
+    author: 'Peter Brown',
+    isbn: '978-0674729018',
+    pages: 336,
+    edition: 1,
+    is_paperback: false
+  },
+  {
+    title: 'Essential Scrum: A Practical Guide to the Most Popular Agile Process',
+    author: 'Kenneth Rubin',
+    isbn: '978-0137043293',
+    pages: 500,
+    edition: 1,
+    is_paperback: true
+  },
+  {
+    title: "White Fragility: Why It's So Hard for White People to Talk About Racism",
+    author: 'Robin DiAngelo',
+    isbn: '978-0807047415',
+    pages: 192,
+    edition: 2,
+    is_paperback: true
+  },
+  {
+    title: 'The Pragmatic Programmer: Your Journey To Mastery',
+    author: 'David Thomas',
+    isbn: '978-0135957059',
+    pages: 352,
+    edition: 2,
+    is_paperback: false
+  },
+  {
+    title: 'The Art of Computer Programming, Vol. 1: Fundamental Algorithms',
+    author: 'Donald Knuth',
+    isbn: '978-0201896831',
+    pages: 672,
+    edition: 3,
+    is_paperback: false
+  },
+  {
+    title: 'Algorithms of Oppression: How Search Engines Reinforce Racism',
+    author: 'Safiya Umoja Noble',
+    isbn: '978-1479837243',
+    pages: 256,
+    edition: 1,
+    is_paperback: true
+  }]
 
-const getBooks = (req, res) => {
-    // TODO: Add a comment describing the functionality of this method
-    Book.findAll().then((bookData) => {
-      res.json(bookData);
-    });
+const getBooks = async(req, res) => {
+    try {
+        const books = await Book.findAll();
+    
+         return  res.json(books);
+    } catch (error) {
+        console.log(`[ERROR]: failed to get books | ${ error.message}`);
+       return  res.status(500).json({success: false, error: error.message});
+    }
   }
 
   const getPaperbacks = (req, res) => {
-    Book.findAll({
-      // TODO: Add a comment describing the functionality of this property
-      order: ['title'],
-      // TODO: Add a comment describing the functionality of this property
-      where: {
-        is_paperback: true
-      },
-      attributes: {
-        // TODO: Add a comment describing the functionality of this property
-        exclude: ['is_paperback', 'edition']
-      }
-    }).then((bookData) => {
-      res.json(bookData);
-    });
+    try {
+        const books = await Book.findAll({
+            where: {
+                is_paperback: true
+              },
+              order: ['title'],
+              attributes: {
+                exclude: ['is_paperback', 'edition']
+              }
+        });
+         return  res.json(books);
+    } catch (error) {
+        console.log(`[ERROR]: failed to get paperback books | ${ error.message}`);
+       return  res.status(500).json({success: false, error: error.message});
+    }
   }
 
   const getBookById = (req, res) => {
-    // TODO: Add a comment describing the functionality of this method
-    Book.findByPk(req.params.id).then((bookData) => {
-      res.json(bookData);
-    });
+
+    try {
+        const book = Book.findByPk(req.params.id);
+        if (book) {
+             return res.json(book);
+        }
+        return res.status(404).json({ success: false, message: "book does not exist"})
+    } catch (error) {
+        console.log(`[ERROR]: failed to get book by id | ${ error.message}`);
+       return  res.status(500).json({success: false, error: error.message});
+    }
   }
 
-  const createBook = (req, res) => {
-    Book.create(req.body)
-      .then((newBook) => {
-        res.json(newBook);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
+  const createBook = async (req, res) => {
+
+    try {
+    const book = req.body;
+
+    const newBook = await Book.create(req.body)
+    
+      return res.json(newBook);
+
+    } catch (error) {
+        console.log(`[ERROR]: failed to create book | ${ error.message}`);
+       return  res.status(500).json({success: false, error: error.message});
+    }
   }
 
+  const seedBooks = (req, res) => {
+    
+    try {
+        await Book.bulkCreate(books)
+      
+        res.send({ success: true });
+    } catch (error) {
+        console.log(`[ERROR]: failed to seed Db | ${ error.message}`);
+       return  res.status(500).json({success: false, error: error.message});
+    }
+  
+  }
   module.exports = {
     getBooks,
     getPaperbacks,
     getBookById,
     createBook,
+    seedBooks
   }
--- models/Book.js
+++ models/Book.js
@@ -1,41 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const connection = require('../config/connection');
 
 class Book extends Model {}
 
-Book.init(
-  {
-    book_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.STRING
-    },
-    author: {
-      type: DataTypes.STRING
-    },
-    isbn: {
-      type: DataTypes.STRING
-    },
-    pages: {
-      type: DataTypes.INTEGER
-    },
-    edition: {
-      type: DataTypes.INTEGER
-    },
-    is_paperback: {
-      type: DataTypes.BOOLEAN
-    }
+const schema =  {
+  book_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'book'
+  title: {
+    type: DataTypes.STRING
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  isbn: {
+    type: DataTypes.STRING
+  },
+  pages: {
+    type: DataTypes.INTEGER
+  },
+  edition: {
+    type: DataTypes.INTEGER
+  },
+  is_paperback: {
+    type: DataTypes.BOOLEAN
   }
-);
+}
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'book'
+}
+
+Book.init(schema, options);
 
 module.exports = Book;
